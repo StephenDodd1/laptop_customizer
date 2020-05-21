ef42d8c9b79a45b69f5d909907f364ee870e1e8a
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-05-21 01:58:18 -0500

    partially rendering Parts Options

--- src/ComponentBox/ComponentBox.js
+++ src/ComponentBox/ComponentBox.js
@@ -3,10 +3,20 @@ import ComponentOptions from '../ComponentOptions/ComponentOptions';
 
 class ComponentBox extends React.Component {
     render() {
+        const arr = [];
+        const listItem = this.props.listHead;
+        const comp=this.props.features;
+        console.log(comp[listItem[0].props.children].length)
+        for(let i = 0; i < 2; i++){
+        arr.push(comp[listItem[i].props.children].name, + ' (', + comp[listItem[i].props.children].cost)
+        }
     return(
+        <ul>
+        <li>{arr}</li>
         <ComponentOptions 
             features={this.props.features} 
         />
+        </ul>
     )}
 }
 
--- src/OptionsBox/OptionsBox.js
+++ src/OptionsBox/OptionsBox.js
@@ -3,11 +3,20 @@ import ComponentBox from '../ComponentBox/ComponentBox';
 
 class OptionsBox extends Component{
     render() {
+        const listHead = Object.keys(this.props.features).map((item,idx)=>
+            <h3 key={idx}>{item}</h3>);
+        console.log(listHead)
         return(
-    <ComponentBox 
-        features={this.props.features}
-        />
-
+            <div>
+                {listHead}
+                <ul>
+                    <ComponentBox 
+                        features={this.props.features}
+                        key={this.props.key}
+                        listHead={listHead}
+                    />
+                </ul>
+            </div>
         )}
 }
 export default OptionsBox;
