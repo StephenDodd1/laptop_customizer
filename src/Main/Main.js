860ac422c2b827e302ad9a60a995b94309287021
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-05-21 00:50:51 -0500

    rendering form header

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,12 +2,41 @@ import React, {Component} from 'react';
 import OptionsBox from '../OptionsBox/OptionsBox';
 
 class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            selected: {
+                Processor: {
+                  name: '17th Generation Intel Core HB (7 Core with donut spare)',
+                  cost: 700
+                },
+                'Operating System': {
+                  name: 'Ubuntu Linux 16.04',
+                  cost: 200
+                },
+                'Video Card': {
+                  name: 'Toyota Corolla 1.5v',
+                  cost: 1150.98
+                },
+                Display: {
+                  name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+                  cost: 1500
+                }
+              }
+        }
+    }
     render() {
-    console.log(this.props.features);
+    console.log(this.state);
     return(
-    <OptionsBox 
-        features={this.props.features}
-    />
+        <main>
+            <form className='main__form'>
+                <h2>Customize your laptop</h2>
+                    <OptionsBox 
+                        features={this.props.features}
+                        state={this.state}
+                    />
+            </form>
+        </main>
       /*  <CartBox />
             <>
             <ComponentDetails />
