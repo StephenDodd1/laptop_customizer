25e43e5844ea22bb8aff9d0afccf27b204b62786
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-05-22 00:40:01 -0500

    Wrapped state in constructor, solved undefined

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ const USCurrencyFormat = new Intl.NumberFormat('en-US', {
 });
 
 class App extends Component {
-  state = {
+  constructor(props) {
+    super(props);
+      this.state = {
     selected: {
       Processor: {
         name: '17th Generation Intel Core HB (7 Core with donut spare)',
@@ -27,12 +29,12 @@ class App extends Component {
       Display: {
         name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
         cost: 1500
-      }
+      }}
     }
   };
 
   updateFeature = (feature, newValue) => {
-    const selected = Object.assign({}, this.state.selected);
+    const selected = Object.assign({}, this.props.selected);
     selected[feature] = newValue;
     this.setState({
       selected
@@ -40,44 +42,10 @@ class App extends Component {
   };
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        
-        return (
-          <div key={itemHash} className="feature__item">
-
-
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
-
+    console.log(this.state.selected)
     const summary = Object.keys(this.state.selected).map((feature, idx) => {
       const featureHash = feature + '-' + idx;
       const selectedOption = this.state.selected[feature];
-
       return (
         <div className="summary__option" key={featureHash}>
           <div className="summary__option__label">{feature} </div>
@@ -97,11 +65,23 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <main>
         <OptionsBox
               features={this.props.features}
               selected={this.state.selected}
+              updateFeature={this.state.updateFeature}
             />
-
+          <section className="main__summary">
+            <h2>Your cart</h2>
+            {summary}
+            <div className="summary__total">
+              <div className="summary__total__label">Total</div>
+              <div className="summary__total__value">
+                {USCurrencyFormat.format(total)}
+              </div>
+            </div>
+          </section>
+        </main>
       </div>
     );
   }
